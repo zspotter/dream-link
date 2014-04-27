53892dd006be1d769c33b367e25875afa38866a9
Zachary Potter <zerothindex@gmail.com>
2014-04-27 17:58:19 -0400

    Spaces to tabs

--- src/app/models.py
+++ src/app/models.py
@@ -4,17 +4,17 @@ from google.appengine.ext import ndb
 class Tag( ndb.Model ): pass
 
 class Dream( ndb.Model ):
-    tags = ndb.KeyProperty(kind=Tag, repeated=True)
+	tags = ndb.KeyProperty(kind=Tag, repeated=True)
 
-    def to_dict( self ):
-        words = []
-        for tag_key in self.tags:
-            words.append(tag_key.get().get_word())
-        return { self.key.urlsafe() : words }
+	def to_dict( self ):
+		words = []
+		for tag_key in self.tags:
+			words.append(tag_key.get().get_word())
+		return { self.key.urlsafe() : words }
 
 
 class Tag( ndb.Model ):
-    dreams = ndb.KeyProperty(kind=Dream, repeated=True)
+	dreams = ndb.KeyProperty(kind=Dream, repeated=True)
 
-    def get_word( self ):
-        return self.key.id()
+	def get_word( self ):
+		return self.key.id()
